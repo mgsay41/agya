3a5fe0a1a40a6e72f87172668e8f6e0a27ee3f6b
omarabdelaal63 <O.mansour.absai@hotmail.com>
2024-11-27 16:34:53 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import UserProfile from "./pages/UserProfile";
 import ChangePhotoComponent from "./components/ChangePhotoComponent";
 import CommentSection from "./components/CommentSection";
 import EditProfile from "./pages/EditProfile";
+import ArticleDetails from "./pages/ArticlesDetails";
+
 
 const App = () => {
   return (
     <div>
-      <Front />
+      <ArticleDetails/>
+      {/* <Front />
       <UserProfile />
       <ChangePhotoComponent />
       <CommentSection />
-      <EditProfile />
+      <EditProfile /> */}
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        main: "#6e2617", // Main Color
+        main: "#31511E", // Main Color
         background: "#fefdfc", // Background Color
         secondary: "#001F34", // Secondary Color
         SoftMain: "#f9eae7", // Soft Main Color
         accent: "#001F34", // Accent Color
+        DateTime: "#777777",
+        textField:"E4F5E4",
+        blackText:"010200",
         "main-font": "#010200", // Main Font Color
         "secondary-font": "#fefdfc", // Secondary Font Color
       },
