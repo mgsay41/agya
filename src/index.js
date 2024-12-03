fab2b60395d6932d98d00c69e0f14f8e9048467d
mahmoudabs2002 <m.mohamed.absai@outlook.com>
2024-12-04 01:06:29 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import { PrimeReactProvider } from "primereact/api";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <PrimeReactProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PrimeReactProvider>
+  </React.StrictMode>
+);
